277e70563ffd5e2d28ebc4447f115353f9f27d85
TrungLam <huynhtrunglam167@gmail.com>
2023-12-05 17:38:46 +0700

    treatment plus

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
     //1: Khai bao cac view de khai thac
@@ -35,8 +38,29 @@ public class MainActivity extends AppCompatActivity {
         mBtnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Click button sum",Toast.LENGTH_SHORT).show();
-            }
+                //Thong bao
+                //Toast.makeText(MainActivity.this, "Click button sum",Toast.LENGTH_SHORT).show();
+
+                //Kiem tra gia tri trong system => System.out
+                //Log.d("BBB","Hello");
+                //Lay gia tri tra ve string
+                String textNumber1 = mTxtInputEdtNumber1.getText().toString();
+                String textNumber2 = mTxtInputEdtNumber2.getText().toString();
+
+                //Kiem tra gia tri
+                if (textNumber1.isEmpty()||textNumber2.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Invalid input",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Chuyen doi tu string sang int
+                int number1 = Integer.parseInt(textNumber1);
+                int number2 = Integer.parseInt(textNumber2);
+
+                int result = number1 + number2;
+                mBvResult.setText("Result: " + result);
+                }
+
         });
     }
 }
